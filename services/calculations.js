ec8b09ba25e8d319774bc8f12e8e9daecb8ec6c5
Gross Jamie <jamie.gross@partner.bmwgroup.com>
2018-10-16 10:00:34 -0400

    Show summaires of calculation

--- services/calculations.js
+++ services/calculations.js
@@ -19,7 +19,6 @@ const calculateTotals = () => {
   let interestPaid = 0;
   let extraPaymentsPaid = 0;
   let totalPaid = 0;
-  let saved = 0;
   const values = [{
     month: numOfMonths,
     monthString: `${months[startDate.getMonth()]}, ${startDate.getFullYear()}`,
@@ -48,6 +47,54 @@ const calculateTotals = () => {
   }
   
   const totalInterest = totalPaid - loanAmount;
+  const saved = totalPaymentNoExtra - totalPaid;
+
+  renderData({ totalPaid, totalInterest, saved, monthlyPayment, numOfMonths }, values);
+};
+
+const renderData = (summary, totals) => {
+  const summaries = createSummariesElement(summary);
+  document.querySelector('#calculations #summaries').remove();
+  document.querySelector('#calculations').append(summaries);
+  document.querySelector('#calculations .empty').style.display = 'none';
+};
+
+const createSummariesElement = (summary) => {
+  const summaries = document.createElement('div');
+  summaries.id = 'summaries';
+
+  const total = document.createElement('span');
+  const totalBold = document.createElement('b').textContent = 'Total: ';
+  const totalSpan = document.createElement('span').textContent = `$${(Math.round(summary.totalPaid * 100) / 100).toFixed(2)}`;
+  total.append(totalBold);
+  total.append(totalSpan);
+  const interest = document.createElement('span');
+  const interestBold = document.createElement('b').textContent = 'Interest: ';
+  const interestSpan = document.createElement('span').textContent = `$${(Math.round(summary.totalInterest * 100) / 100).toFixed(2)}`;
+  interest.append(interestBold);
+  interest.append(interestSpan);
+  const saved = document.createElement('span');
+  const savedBold = document.createElement('b').textContent = 'Saved: ';
+  const savedSpan = document.createElement('span').textContent = `$${(Math.round(summary.saved * 100) / 100).toFixed(2)}`;
+  saved.append(savedBold);
+  saved.append(savedSpan);
+  const monthly = document.createElement('span');
+  const monthlyBold = document.createElement('b').textContent = 'Monthly Payment: ';
+  const monthlySpan = document.createElement('span').textContent = `$${(Math.round(summary.monthlyPayment * 100) / 100).toFixed(2)}`;
+  monthly.append(monthlyBold);
+  monthly.append(monthlySpan);
+  const years = document.createElement('span');
+  const yearsBold = document.createElement('b').textContent = 'Years: ';
+  const yearsSpan = document.createElement('span').textContent = `${(Math.round((summary.numOfMonths / 12) * 10) / 10).toFixed(1)}`;
+  years.append(yearsBold);
+  years.append(yearsSpan);
+
+  summaries.append(total);
+  summaries.append(interest);
+  summaries.append(saved);
+  summaries.append(monthly);
+  summaries.append(years);
+  return summaries;
 };
 
 module.exports = { calculateTotals };
